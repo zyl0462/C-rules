205627928458fcc4bbc29bcc6dae56566ac05ec6
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2023-06-01 19:04:54 +0800

    Update loon.py

--- action/loon.py
+++ action/loon.py
@@ -4,31 +4,11 @@ rawDirect = requests.get("https://raw.githubusercontent.com/Code-Dramatist/Rule_
 rawProxy = requests.get("https://raw.githubusercontent.com/Code-Dramatist/Rule_Actions/main/Proxy_Rule/Proxy_Rule.rule").text
 rawReject = requests.get("https://raw.githubusercontent.com/Code-Dramatist/Rule_Actions/main/Reject_Rule/Reject_Rule.rule").text
 
-result = list()
-for rawresult in rawDirect:
-    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
-result_text = '\n'.join(result)
-
 with open("./Loon/direct.txt", "w") as f:
-    f.write("\n".join(result_text))
- 
-result.clear()
-
-for rawresult in rawProxy:
-    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
-result_text = '\n'.join(resul)
+    f.write(rawDirect)
 
 with open("./Loon/proxy.txt", "w") as f:
-    f.write("\n".join(result_text))
- 
-result.clear()
-
-for rawresult in rawReject:
-    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
-result_text = '\n'.join(result)
+    f.write(rawProxy)
 
 with open("./Loon/reject.txt", "w") as f:
-    f.write("\n".join(result_text))
- 
-result.clear()
-
+    f.write(rawReject)
