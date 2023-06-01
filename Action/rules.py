73f2690ef954bd13ddf91ab5f82574f91f27e9ea
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2023-06-01 19:26:14 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -1,1 +1,24 @@
+import requests
 
+directRule = requests.get("https://raw.githubusercontent.com/Code-Dramatist/Rule_Actions/main/Direct_Rule/Direct_Rule.rule").text
+proxyRule = requests.get("https://raw.githubusercontent.com/Code-Dramatist/Rule_Actions/main/Proxy_Rule/Proxy_Rule.rule").text
+rejectRule = requests.get("https://raw.githubusercontent.com/Code-Dramatist/Rule_Actions/main/Reject_Rule/Reject_Rule.rule").text
+
+
+directModule = requests.get("https://raw.githubusercontent.com/GMOogway/shadowrocket-rules/master/sr_direct_list.module").text
+proxyModule = requests.get("https://raw.githubusercontent.com/GMOogway/shadowrocket-rules/master/sr_proxy_list.module").text
+rejectModule = requests.get("https://raw.githubusercontent.com/GMOogway/shadowrocket-rules/master/sr_reject_list.module").text
+
+with open("./Rules/direct.txt", "w") as f:
+    f.write(directRule)
+with open("./Rules/proxy.txt", "w") as f:
+    f.write(proxyRule)
+with open("./Rules/reject.txt", "w") as f:
+    f.write(rejectRule)
+
+with open("./Rules/direct.module", "w") as f:
+    f.write(directModule)
+with open("./Rules/proxy.module", "w") as f:
+    f.write(proxyModule)
+with open("./Rules/reject.module", "w") as f:
+    f.write(rejectModule)
