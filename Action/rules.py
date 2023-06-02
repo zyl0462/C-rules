44bcc0d0ce4ead56854980bd5e1a4cf5e07b4071
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2023-06-02 19:09:41 +0800

    更新 rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -12,19 +12,19 @@ del(r_rules)
 with open("./Rules/reject.txt", "w") as f:
     f.write(reject_text)
 
-Proxy_Rules = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Proxy/Proxy_Domain.list").text
-Proxy_Rules1 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Global/Global_Domain.list").text
-Proxy_Rules2 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/GlobalMedia/GlobalMedia_Domain.list").text
-Proxy_Rules3 = requests.get("https://raw.githubusercontent.com/Loyalsoldier/surge-rules/release/gfw.txt").text
-Proxy_Rules4 = requests.get("https://raw.githubusercontent.com/Loyalsoldier/surge-rules/release/proxy.txt").text
-Proxy_Rules5 = requests.get("https://raw.githubusercontent.com/Loyalsoldier/surge-rules/release/tld-not-cn.txt").text
+#Proxy_Rules = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Proxy/Proxy_Domain.list").text
+#Proxy_Rules1 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Global/Global_Domain.list").text
+#Proxy_Rules2 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/GlobalMedia/GlobalMedia_Domain.list").text
+Proxy_Rules = requests.get("https://raw.githubusercontent.com/Loyalsoldier/surge-rules/release/gfw.txt").text
+Proxy_Rules1 = requests.get("https://raw.githubusercontent.com/Loyalsoldier/surge-rules/release/proxy.txt").text
+#Proxy_Rules5 = requests.get("https://raw.githubusercontent.com/Loyalsoldier/surge-rules/release/tld-not-cn.txt").text
 
 p_rules= set([item for item in Proxy_Rules.strip().split("\n") if not item.startswith('#')])
 p_rules.update([item for item in Proxy_Rules1.strip().split("\n") if not item.startswith('#')])
-p_rules.update([item for item in Proxy_Rules2.strip().split("\n") if not item.startswith('#')])
-p_rules.update([item for item in Proxy_Rules3.strip().split("\n") if not item.startswith('#')])
-p_rules.update([item for item in Proxy_Rules4.strip().split("\n") if not item.startswith('#')])
-p_rules.update([item for item in Proxy_Rules5.strip().split("\n") if not item.startswith('#')])
+#p_rules.update([item for item in Proxy_Rules2.strip().split("\n") if not item.startswith('#')])
+#p_rules.update([item for item in Proxy_Rules3.strip().split("\n") if not item.startswith('#')])
+#p_rules.update([item for item in Proxy_Rules4.strip().split("\n") if not item.startswith('#')])
+#p_rules.update([item for item in Proxy_Rules5.strip().split("\n") if not item.startswith('#')])
 
 proxy_text = '\n'.join(sorted(p_rules))
 del(p_rules)
