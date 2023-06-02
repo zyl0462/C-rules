c04405ef789654fc8a62229cb3e3ef6a10127624
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2023-06-02 17:41:30 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -3,22 +3,34 @@ import requests
 Reject1_Rules = requests.get("https://anti-ad.net/domains.txt").text
 Reject2_Rules = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/EasyPrivacy/EasyPrivacy_Domain.list").text
 
-r_rules1= set([item for item in Reject1_Rules.strip().split("\n") if not item.startswith('#')])
-r_rules2= set([item for item in Reject2_Rules.strip().split("\n") if not item.startswith('#')])
+r_rules= set([item for item in Reject1_Rules.strip().split("\n") if not item.startswith('#')])
+r_rules1= set([item for item in Reject2_Rules.strip().split("\n") if not item.startswith('#')])
+r_rules.symmetric_difference_update(r_rules1)	
+reject_text = '\n'.join(sorted(r_rules))
 
-r_rules = r_rules1.union(r_rules2)
 del(r_rules1)
 del(r_rules2)
-reject_text = '\n'.join(sorted(r_rules))
+del(r_rules)
+
 with open("./Rules/reject.txt", "w") as f:
     f.write(reject_text)
 
-del(r_rules)
 
+GFW_rules = requests.get("https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/gfw.txt").text  
+TLD_rules = requests.get("https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/tld-not-cn.txt").text  
+TELEGRAM_rules = requests.get("https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/telegramcidr.txt").text
 
-GFW_rules = requests.get("https://raw.githubusercontent.com/Loyalsoldier/surge-rules/release/ruleset/gfw.txt").text  
-TLD_rules = requests.get("https://raw.githubusercontent.com/Loyalsoldier/surge-rules/release/ruleset/tld-not-cn.txt").text  
-TELEGRAM_rules = requests.get("https://raw.githubusercontent.com/Loyalsoldier/surge-rules/release/ruleset/telegramcidr.txt").text  
+p_rules= set([item for item in Reject1_Rules.strip().split("\n") if not (item.startswith('#') or item.startswith('payload:'))])
+p_rules1= set([item for item in Reject2_Rules.strip().split("\n") if not (item.startswith('#') or item.startswith('payload:'))])
+p_rules.symmetric_difference_update(p_rules1)
+list = sorted(p_rules)
+list.insert(0,'payload:')
+proxy_text = '\n'.join(list)
+
+with open("./Rules/proxy.yaml", "w") as f:
+    f.write(proxy_text)
+with open("./Rules/telegramcidr.yaml", "w") as f:
+    f.write(TELEGRAM_rules)
 
 
 
