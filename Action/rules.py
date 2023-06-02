eeb3afd7b21632be3379a15a8d4b390958946dbd
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2023-06-02 18:33:10 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -1,5 +1,6 @@
 import requests
 
+'''reject rules
 Reject1_Rules = requests.get("https://anti-ad.net/domains.txt").text
 Reject2_Rules = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/EasyPrivacy/EasyPrivacy_Domain.list").text
 
@@ -14,28 +15,47 @@ del(r_rules)
 with open("./Rules/reject.txt", "w") as f:
     f.write(reject_text)
 
+'''proxy rules
+Proxy_Rules = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Proxy/Proxy_Domain.list").text
+Proxy_Rules1 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Global/Global_Domain.list").text
+Proxy_Rules2 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/GlobalMedia/GlobalMedia_Domain.list").text
 
+p_rules= set([item for item in Proxy_Rules.strip().split("\n") if not item.startswith('#')])
+p_rules1= set([item for item in Proxy_Rules1.strip().split("\n") if not item.startswith('#')])
+p_rules2= set([item for item in Proxy_Rules2.strip().split("\n") if not item.startswith('#')])
+p_rules.symmetric_difference_update(p_rules1)
+p_rules.symmetric_difference_update(p_rules2)	
+proxy_text = '\n'.join(sorted(p_rules))
+
+del(p_rules)
+del(p_rules1)
+del(p_rules2)
+
+with open("./Rules/proxy.txt", "w") as f:
+    f.write(proxy_text)
+
+'''clash rules
 GFW_rules = requests.get("https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/gfw.txt").text  
 TLD_rules = requests.get("https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/tld-not-cn.txt").text  
 TELEGRAM_rules = requests.get("https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/telegramcidr.txt").text
 
-p_rules= set([item for item in GFW_rules.split("\n") if not (item.startswith('#') or item.startswith('payload:'))])
-p_rules1= set([item for item in TLD_rules.split("\n") if not (item.startswith('#') or item.startswith('payload:'))])
-p_rules.symmetric_difference_update(p_rules1)
-lists = sorted(p_rules)
+c_rules= set([item for item in GFW_rules.split("\n") if not (item.startswith('#') or item.startswith('payload:'))])
+c_rules1= set([item for item in TLD_rules.split("\n") if not (item.startswith('#') or item.startswith('payload:'))])
+c_rules.symmetric_difference_update(c_rules1)
+lists = sorted(c_rules)
 lists.insert(0,'payload:')
-proxy_text = '\n'.join(lists)
+c_text = '\n'.join(lists)
 del(p_rules1)
 del(p_rules)
 lists.clear()
 
 with open("./Rules/proxy.yaml", "w") as f:
-    f.write(proxy_text)
+    f.write(c_text)
 with open("./Rules/telegramcidr.yaml", "w") as f:
     f.write(TELEGRAM_rules)
 
 
-
+'''shadowrocket rules
 directModule = requests.get("https://raw.githubusercontent.com/GMOogway/shadowrocket-rules/master/sr_direct_list.module").text
 proxyModule = requests.get("https://raw.githubusercontent.com/GMOogway/shadowrocket-rules/master/sr_proxy_list.module").text
 rejectModule = requests.get("https://raw.githubusercontent.com/GMOogway/shadowrocket-rules/master/sr_reject_list.module").text
