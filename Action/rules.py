9b8f966115228cec46f0612e8946f2ca48f5f8e5
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2023-06-09 16:02:30 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -6,10 +6,10 @@ Reject2_Rules = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios
 
 r_rules= set([item for item in Reject1_Rules.strip().split("\n") if not item.startswith('#')])
 r_rules.update([item for item in Reject2_Rules.strip().split("\n") if not item.startswith('#')])
+LEN_reject = len(r_rules)
 
 reject_text = '\n'.join(sorted(r_rules))
 del(r_rules)
-
 with open("./Rules/reject.txt", "w") as f:
     f.write(reject_text)
 
@@ -21,10 +21,13 @@ p_rules= set([item for item in Proxy_Rules.strip().split("\n") if not item.start
 p_rules.update([item for item in Proxy_Rules1.strip().split("\n") if not item.startswith('#')])
 p_rules.update([item for item in Proxy_Rules2.strip().split("\n") if not item.startswith('#')])
 p_rules.update([item for item in Proxy_Rules3.strip().split("\n") if not item.startswith('#')])
+LEN_proxy = len(p_rules)
+
 proxy_text = '\n'.join(sorted(p_rules))
 del(p_rules)
 with open("./Rules/proxy.txt", "w") as f:
     f.write(proxy_text)
+
 p_rules= [item for item in proxy_text.strip().split("\n") if not item.startswith('.')]
 p_rules1= [item[1:] for item in proxy_text.strip().split("\n") if  item.startswith('.')]
 p_text = ',PROXY\n'.join(p_rules)
@@ -48,10 +51,9 @@ p_text ='\n'.join(sorted(p_rules))
 p_text1 ='\n'.join(sorted(p_rules1))
 del(p_rules)
 del(p_rules1)
-current_time = '#' + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + '\n'
 
 with open("./Rules/rproxy.text", "w") as f:
-    f.write(current_time + p_text + '\n' + p_text1)
+    f.write(p_text + '\n' + p_text1)
 
 Social_Rules = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Telegram/Telegram_Resolve.list").text
 Social_Rules1 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/OpenAI/OpenAI.list").text
@@ -59,6 +61,8 @@ Social_Rules2 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios
 s_rules= set([item for item in Social_Rules.strip().split("\n") if not item.startswith('#')])
 s_rules.update([item for item in Social_Rules1.strip().split("\n") if not item.startswith('#')])
 s_rules.update([item for item in Social_Rules2.strip().split("\n") if not item.startswith('#')])
+LEN_social= len(s_rules)
+
 social_text = '\n'.join(sorted(s_rules))
 del(s_rules)
 with open("./Rules/social.txt", "w") as f:
@@ -71,7 +75,11 @@ Video_Rules = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_r
 Video_Rules1 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/YouTube/YouTube_Resolve.list").text
 v_rules= set([item for item in Video_Rules.strip().split("\n") if not item.startswith('#')])
 v_rules.update([item for item in Video_Rules1.strip().split("\n") if not item.startswith('#')])
+LEN_video= len(v_rules)
+
 video_text = '\n'.join(sorted(v_rules))
 del(v_rules)
 with open("./Rules/video.txt", "w") as f:
     f.write(video_text)
+
+current_time = '#' + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + '\n'
