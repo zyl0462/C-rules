de25259508b6dd9149b90628b615a3730e67209e
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2023-06-09 16:14:21 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -82,7 +82,7 @@ del(v_rules)
 with open("./Rules/video.txt", "w") as f:
     f.write(video_text)
 
-current_time = '#' + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + '\n'
-STR_stat = "reject rules: {};\n proxy rules: {};\n social rules: {};\n video rules: {};\n".format(LEN_reject, LEN_proxy, LEN_social,LEN_video)
-with open("./Rules/stat.txt", "w") as f:
+current_time = '#UTC time: ' + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + '\n'
+STR_stat = "reject rules: {}\nproxy rules: {}\nsocial rules: {}\nvideo rules: {}\n".format(LEN_reject, LEN_proxy, LEN_social,LEN_video)
+with open("./Rules/stat.dat", "w") as f:
     f.write(current_time + STR_stat)
