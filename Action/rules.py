81c758dbeed6ffbcf7c92e3c5bbc85003cd84aea
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2023-07-21 10:17:01 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -2,11 +2,11 @@ import requests
 from datetime import datetime
 
 def get_text(url):
-     response = requests.get(url)
-     if response.status_code == 200:
-          return response.text
-     else:
-          return None
+  response = requests.get(url)
+  if response.status_code == 200:
+    return response.text
+  else:
+    return None
 
 Reject_Rules = get_text("https://anti-ad.net/domains.txt")
 Reject_Rules1 = get_text("https://raw.githubusercontent.com/Cats-Team/AdRules/main/ad-domains.txt").lower()
@@ -17,7 +17,7 @@ r_rules.update([item[2:-1] for item in Reject_Rules2.strip().split("\n") if (ite
 LEN_reject = len(r_rules)
 reject_text = '\n'.join(sorted(r_rules))
 with open("./Rules/reject.txt", "w",encoding='utf-8') as f:
-    f.write(reject_text)
+  f.write(reject_text)
 del Reject_Rules,Reject_Rules1,Reject_Rules2,r_rules,reject_text
 
 Proxy_Rules = get_text("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Proxy/Proxy_Domain.list")
@@ -29,14 +29,14 @@ p_rules.update([item for item in Proxy_Rules2.strip().split("\n") if not item.st
 LEN_proxy = len(p_rules)
 proxy_text = '\n'.join(sorted(p_rules))
 with open("./Rules/proxy.txt", "w",encoding='utf-8') as f:
-    f.write(proxy_text)
+  f.write(proxy_text)
 del Proxy_Rules,Proxy_Rules1,Proxy_Rules2,p_rules
 
 p_rules= [('DOMAIN,' + item + ',PROXY') for item in proxy_text.strip().split("\n") if not item.startswith('.')]
 p_rules.extend([('DOMAIN-SUFFIX,'+ item[1:] + ',PROXY') for item in proxy_text.strip().split("\n") if item.startswith('.')])
 p_text ='\n'.join(sorted(p_rules))
 with open("./Rules/rproxy.txt", "w",encoding='utf-8') as f:
-    f.write(p_text)
+  f.write(p_text)
 del p_rules, proxy_text, p_text
 
 Social_Rules = get_text("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Telegram/Telegram.list")
@@ -48,7 +48,7 @@ s_rules.update([item for item in Social_Rules2.strip().split("\n") if not item.s
 LEN_social= len(s_rules)
 social_text = '\n'.join(sorted(s_rules))
 with open("./Rules/social.txt", "w",encoding='utf-8') as f:
-    f.write(social_text)
+  f.write(social_text)
 del s_rules,Social_Rules,Social_Rules1,Social_Rules2,social_text
 
 Video_Rules = get_text("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/TikTok/TikTok.list")
@@ -58,12 +58,12 @@ v_rules.update([item for item in Video_Rules1.strip().split("\n") if not item.st
 LEN_video= len(v_rules)
 video_text = '\n'.join(sorted(v_rules))
 with open("./Rules/video.txt", "w",encoding='utf-8') as f:
-    f.write(video_text)
+  f.write(video_text)
 del Video_Rules,Video_Rules1,v_rules,video_text
 
 STR_stat = f'#UTC time: {datetime.now().strftime("%Y/%m/%d %H:%M:%S")}\nreject rules: {LEN_reject}\nproxy rules: {LEN_proxy}\nsocial rules: {LEN_social}\nvideo rules: {LEN_video}\ntotal rules: {LEN_reject+LEN_proxy+LEN_social+LEN_video}'
 with open("./stat", "w",encoding='utf-8') as f:
-    f.write(STR_stat)
+  f.write(STR_stat)
 del STR_stat
 
 Social_Rules = get_text("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Telegram/Telegram_No_Resolve.yaml")
@@ -74,7 +74,7 @@ s_rules.update([item for item in Social_Rules1.strip().split("\n") if not (item.
 s_rules.update([item for item in Social_Rules2.strip().split("\n") if not (item.startswith('#') or item.startswith('payload:'))])
 social_text = '\n'.join(sorted(s_rules))
 with open("./Rules/social.yaml", "w",encoding='utf-8') as f:
-    f.write('payload:\n'+social_text)
+  f.write('payload:\n'+social_text)
 del Social_Rules,Social_Rules1,Social_Rules2,s_rules,social_text
 
 Video_Rules = get_text("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/YouTube/YouTube_No_Resolve.yaml")
@@ -83,5 +83,5 @@ v_rules= set([item for item in Video_Rules.strip().split("\n") if not (item.star
 v_rules.update([item for item in Video_Rules1.strip().split("\n") if not (item.startswith('#') or item.startswith('payload:'))])
 video_text = '\n'.join(sorted(v_rules))
 with open("./Rules/video.yaml", "w",encoding='utf-8') as f:
-    f.write('payload:\n'+video_text)
+  f.write('payload:\n'+video_text)
 del Video_Rules,Video_Rules1,v_rules,video_text
