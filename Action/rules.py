888c526e0d7498c013d186e5433e5b731d2edec0
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2023-07-24 10:52:10 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -3,7 +3,7 @@ import sys
 from datetime import datetime
 
 def get_text(url):
-    response = requests.get(url, stream=true)
+    response = requests.get(url, stream=True)
     if response.status_code == 200:
         with open("./tmp", "wb+",encoding='utf-8') as f:
           for chunk in r.iter_content(chunk_size=512):
