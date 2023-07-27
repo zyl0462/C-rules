38297aca3d31b34acaf98bb068884eb8ce328a1e
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2023-07-27 10:33:18 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -14,34 +14,36 @@ def get_text(url):
         else:
             sys.exit(0)
 
-REJECT_URL = ("https://anti-ad.net/domains.txt",
-         "https://raw.githubusercontent.com/Cats-Team/AdRules/main/ad-domains.txt",
-         "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt")
-PROXY_URL = ("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Proxy/Proxy_Domain.list",
-             "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/GlobalMedia/GlobalMedia_Domain.list",
-             "https://raw.githubusercontent.com/Loyalsoldier/surge-rules/release/gfw.txt")
-SOCIAL_URL = ("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Telegram/Telegram.list",
-              "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/OpenAI/OpenAI.list",
-              "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Google/Google.list")
-VIDEO_URL = ("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/TikTok/TikTok.list",
-             "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/YouTube/YouTube.list")
-SOCIAL_YMAL = ("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Telegram/Telegram_No_Resolve.yaml",
-               "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/OpenAI/OpenAI.yaml",
-              "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Google/Google_No_Resolve.yaml")
-VIDEO_YMAL = ("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/YouTube/YouTube_No_Resolve.yaml",
-              "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/TikTok/TikTok_No_Resolve.yaml")
+RULES_URL =(
+            ("https://anti-ad.net/domains.txt",
+                "https://raw.githubusercontent.com/Cats-Team/AdRules/main/ad-domains.txt",
+                "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt"),
+            ("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Proxy/Proxy_Domain.list",
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/GlobalMedia/GlobalMedia_Domain.list",
+                "https://raw.githubusercontent.com/Loyalsoldier/surge-rules/release/gfw.txt"),
+            ("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Telegram/Telegram.list",
+                 "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/OpenAI/OpenAI.list",
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Google/Google.list"),
+            ("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/TikTok/TikTok.list",
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/YouTube/YouTube.list"),
+            ("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Telegram/Telegram_No_Resolve.yaml",
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/OpenAI/OpenAI.yaml",
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Google/Google_No_Resolve.yaml"),
+            ("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/YouTube/YouTube_No_Resolve.yaml",
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/TikTok/TikTok_No_Resolve.yaml")
+)
 
-reject_set = set([i for i in get_text(REJECT_URL[0]).split("\n") if not (i.startswith('#') or i.startswith('!'))])
-reject_set .update([i for i in get_text(REJECT_URL[1]).lower().split("\n") if not (i.startswith('#') or i.startswith('!'))])
-reject_set .update([i[2:-1] for i in get_text(REJECT_URL[2]).split("\n") if (i.startswith('||') and i.endswith('^'))])
+reject_set = set([i for i in get_text(RULES_URL[0][0]).split("\n") if not (i.startswith('#') or i.startswith('!'))])
+reject_set .update([i for i in get_text(RULES_URL[0][1]).lower().split("\n") if not (i.startswith('#') or i.startswith('!'))])
+reject_set .update([i[2:-1] for i in get_text(RULES_URL[0][2]).split("\n") if (i.startswith('||') and i.endswith('^'))])
 LEN_reject = len(reject_set)
 reject_text = '\n'.join(sorted(reject_set))
 with open("./Rules/reject.txt", "w",encoding='utf-8') as f:
     f.write(reject_text)
-del reject_set,reject_text,REJECT_URL
+del reject_set,reject_text
 
 proxy_set =set()
-for item in PROXY_URL:
+for item in RULES_URL[1]:
     proxy_set.update([i for i in get_text(item).split("\n") if not i.startswith('#')])
 LEN_proxy = len(proxy_set)
 proxy_text = '\n'.join(sorted(proxy_set))
@@ -52,29 +54,28 @@ p_rules.extend([('DOMAIN-SUFFIX,'+ i[1:] + ',PROXY') for i in proxy_text.strip()
 p_text ='\n'.join(sorted(p_rules))
 with open("./Rules/rproxy.txt", "w",encoding='utf-8') as f:
     f.write(p_text)
-del p_rules, proxy_text, p_text,proxy_set,PROXY_URL
+del p_rules, proxy_text, p_text,proxy_set
 
 social_set = set()
-for item in SOCIAL_URL:
+for item in RULES_URL[2]:
     social_set.update([i for i in get_text(item).split("\n") if not i.startswith('#')])
 LEN_social= len(social_set)
 social_text = '\n'.join(sorted(social_set))
 with open("./Rules/social.txt", "w",encoding='utf-8') as f:
     f.write(social_text)
 video_set = set()
-for item in VIDEO_URL:
+for item in RULES_URL[3]:
     video_set.update([i for i in get_text(item).split("\n") if not i.startswith('#')])
 LEN_video= len(video_set)
 video_text = '\n'.join(sorted(video_set))
 with open("./Rules/video.txt", "w",encoding='utf-8') as f:
     f.write(video_text)
-del video_set,video_text,VIDEO_URL,social_set,social_text,SOCIAL_URL
+del video_set,video_text,social_set,social_text
 
 my_stat = []
 with open("./stat", "r",encoding='utf-8') as f:
     my_stat.extend([i[i.rindex(' ')+1:] for i in f.read().strip().split("\n") if not i.startswith('#')])
 LEN_reject0,LEN_proxy0,LEN_social0,LEN_video0,LEN_total= int(my_stat[0]),int(my_stat[1]),int(my_stat[2]),int(my_stat[3]),int(my_stat[4])
-
 STR_stat = f'#{(datetime.utcnow().astimezone(timezone(timedelta(hours=8)))).strftime("%Y/%m/%d %H:%M:%S")}\n\
 reject rules({LEN_reject-LEN_reject0}): {LEN_reject}\nproxy rules({LEN_proxy-LEN_proxy0}): {LEN_proxy}\n\
 social rules({LEN_social-LEN_social0}): {LEN_social}\nvideo rules({LEN_video-LEN_video0}): {LEN_video}\n\
@@ -84,15 +85,15 @@ with open("./stat", "w",encoding='utf-8') as f:
 del my_stat,STR_stat,LEN_reject,LEN_proxy,LEN_social,LEN_video,LEN_reject0,LEN_proxy0,LEN_social0,LEN_video0,LEN_total
 
 social_ymal_set = set()
-for item in SOCIAL_YMAL:
+for item in RULES_URL[4]:
     social_ymal_set.update([i for i in get_text(item).split("\n") if not (i.startswith('#') or i.startswith('payload:'))])
 social_ymal = '\n'.join(sorted(social_ymal_set))
 with open("./Rules/social.yaml", "w",encoding='utf-8') as f:
     f.write('payload:\n'+social_ymal)
 video_ymal_set = set()
-for item in VIDEO_YMAL:
+for item in RULES_URL[5]:
     video_ymal_set.update([i for i in get_text(item).split("\n") if not (i.startswith('#') or i.startswith('payload:'))])
 video_ymal = '\n'.join(sorted(video_ymal_set))
 with open("./Rules/video.yaml", "w",encoding='utf-8') as f:
     f.write('payload:\n'+video_ymal)
-del video_ymal_set,video_ymal,VIDEO_YMAL,social_ymal_set,social_ymal,SOCIAL_YMAL
+del video_ymal_set,video_ymal,social_ymal_set,social_ymal,RULES_URL
