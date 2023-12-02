7bdd4f5423461f1917101d48d79d4dd2b8216db3
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2023-12-02 11:47:04 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -48,14 +48,11 @@ for item in RULES_URL[1]:
     proxy_set.update([i for i in get_text(item).split("\n") if not i.startswith('#')])
 LEN_proxy = len(proxy_set)
 proxy_text = '\n'.join(sorted(proxy_set))
-with open("./Rules/proxy.txt", "w",encoding='utf-8') as f:
-    f.write(proxy_text)
-p_rules= [('DOMAIN,' + i + ',PROXY') for i in proxy_text.strip().split("\n") if not i.startswith('.')]
-p_rules.extend([('DOMAIN-SUFFIX,'+ i[1:] + ',PROXY') for i in proxy_text.strip().split("\n") if i.startswith('.')])
+p_rules= [('DOMAIN,' + i) for i in proxy_text.strip().split("\n") if not i.startswith('.')]
+p_rules.extend([('DOMAIN-SUFFIX,'+ i[1:]) for i in proxy_text.strip().split("\n") if i.startswith('.')])
 p_text ='\n'.join(sorted(p_rules))
-with open("./Rules/rproxy.txt", "w",encoding='utf-8') as f:
+with open("./Rules/proxy.txt", "w",encoding='utf-8') as f:
     f.write(p_text)
-
 del p_rules, proxy_text, p_text,proxy_set
 
 social_set = set()
