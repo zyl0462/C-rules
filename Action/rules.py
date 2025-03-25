903f9690665fc361d6944e2bb059f96bfe55e975
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2025-03-26 01:04:28 +0800

    Update rules.py

--- Action/rules.py
+++ Action/rules.py
@@ -46,7 +46,7 @@ DIRECT_URL = ('https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/ma
              )
 reject_set = set([i for i in get_text(REJECT_URL[0]).split("\n") if not (i.startswith('#') or i.startswith('!'))])
 reject_set.update([i[2:-1] for i in get_text(REJECT_URL[1]).split("\n") if (i.startswith('||') and i.endswith('^'))])
-
+LEN_reject = len(reject_set)
 reject_text = '\n'.join(sorted(reject_set))
 with open("./Rules/reject.txt", "w",encoding='utf-8') as f:
     f.write(reject_text)
@@ -55,12 +55,14 @@ del reject_set,reject_text
 proxy_set = set()
 for item in PROXY_URL[0]:
     proxy_set.update([i for i in get_text(item).split("\n") if not (i.startswith('#') or i.startswith('!'))])
+LEN_proxy_domain = len(proxy_set)
 proxy_text = '\n'.join(sorted(proxy_set))
 with open("./Rules/proxy.txt", "w",encoding='utf-8') as f:
     f.write(proxy_text)
 proxy_set.clear()
 for item in PROXY_URL[1]:
     proxy_set.update([i for i in get_text(item).split("\n") if not (i.startswith('#') or i.startswith('payload:'))])
+LEN_proxy = len(proxy_set)
 proxy_text = 'payload:\n' + '\n'.join(sorted(proxy_set))
 with open("./Rules/proxy.yaml", "w",encoding='utf-8') as f:
     f.write(proxy_text)
@@ -69,7 +71,22 @@ del proxy_set,proxy_text
 direct_set = set()
 for item in DIRECT_URL:
     direct_set.update([i for i in get_text(item).split("\n") if not (i.startswith('#') or i.startswith('payload:'))])
+LEN_direct = len(direct_set)
 direct_text = 'payload:\n' + '\n'.join(sorted(direct_set))
 with open("./Rules/direct.yaml", "w",encoding='utf-8') as f:
     f.write(direct_text)
 del direct_set,direct_text
+
+my_stat = []
+with open("./Rules/stat", "r",encoding='utf-8') as f:
+    my_stat.extend([i[i.rindex(' ')+1:] for i in f.read().strip().split("\n") if not i.startswith('#')])
+LEN_reject0,LEN_proxy_domain0,LEN_proxy0,LEN_direct0,LEN_total= int(my_stat[0]),int(my_stat[1]),int(my_stat[2]),int(my_stat[3]),int(my_stat[4])
+STR_stat = f'#{(datetime.now().astimezone(timezone(timedelta(hours=8)))).strftime("%Y/%m/%d %H:%M:%S")}(UTC/GMT+08:00)\n\
+reject rules({LEN_reject0}{"+" if LEN_reject-LEN_reject0 >= 0 else "-"}{abs(LEN_reject-LEN_reject0)}): {LEN_reject}\n\
+proxy-domain rules({LEN_proxy_domain0}{"+" if LEN_proxy_domain-LEN_proxy_domain0 >= 0 else "-"}{abs(LEN_proxy_domain-LEN_proxy_domain0)}): {LEN_proxy_domain}\n\
+proxy rules({LEN_proxy0}{"+" if LEN_proxy-LEN_proxy0 >= 0 else "-"}{abs(LEN_proxy-LEN_proxy0)}): {LEN_proxy}\n\
+direct rules({LEN_direct0}{"+" if LEN_direct-LEN_direct0 >= 0 else "-"}{abs(LEN_direct-LEN_direct0)}): {LEN_direct}\n\
+total rules({LEN_total}{"+" if LEN_reject+LEN_proxy_domain+LEN_proxy+LEN_direct-LEN_total >= 0 else "-"}{abs(LEN_reject+LEN_proxy_domain+LEN_proxy+LEN_direct-LEN_total)}): {LEN_reject+LEN_proxy_domain+LEN_proxy+LEN_direct}'
+with open("./Rules/stat", "w",encoding='utf-8') as f:
+    f.write(STR_stat)
+del my_stat,STR_stat,LEN_reject,LEN_proxy_domain,LEN_proxy,LEN_direct,LEN_reject0,LEN_proxy_domain0,LEN_proxy0,LEN_direct0,LEN_total
